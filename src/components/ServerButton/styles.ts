6b31736a0308eb33afdf0017036bfb5d05752d5d
matheus souza <ruivorobado@gmail.com>
2021-10-07 17:27:30 -0300

    numero de notificacoes dos servidores adicionada

--- src/components/ServerButton/styles.ts
+++ src/components/ServerButton/styles.ts
@@ -31,7 +31,24 @@ export const Button = styled.button<Props>`
     display: ${props => (props.hasNotifications ? 'inline' : 'none')};
   }
   &::after{
+    background-color: var(--notification);
+    width: auto;
+    height: 16px;
 
+    padding: 0 4px;
+    position: absolute;
+    bottom: -4px;
+    right: -4px;
+
+    border-radius: 12px;
+    border: 4px solid var(quaternary);
+    text-align: right;
+    font-size: 13px;
+    font-weight: bold;
+    color: var(--white);
+
+    content: '${props => props.mentions && props.mentions}';
+    display: ${props => props.mentions && props.mentions > 0 ? 'inline' : 'none'};
   }
   transition: border-radius .2s, background-color .2s;
   &.active, &:hover{
