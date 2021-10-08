0d508c6e88b1f3247e12353a60531ac6846d024f
matheus souza <ruivorobado@gmail.com>
2021-10-08 19:25:25 -0300

    iniciando estilizacao da lista de usuarios

--- src/components/UserList/styles.ts
+++ src/components/UserList/styles.ts
@@ -2,6 +2,22 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   grid-area: UL;
+  flex-direction:column;
+  padding: 3px 6px 0 16px;
+  background-color:var(--secondary);
+  max-height:calc(100vh - 46px);
+  overflow-y:scroll;
+  ::-webkit-scrollbar{
+    width:4px;
+  }
+  ::-webkit-scrollbar-thumb{
+    background-color: var(--tertiary);
+    border-radius:4px;
+
+  }
+  ::-webkit-scrollbar-track{
+    background-color:var(--secondary);
+  }
 `;
 export const Role = styled.div`
   
