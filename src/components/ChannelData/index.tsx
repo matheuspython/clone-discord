c758ccf532dacdc01dc6328611a9a434944b5cd1
matheus souza <ruivorobado@gmail.com>
2021-10-09 08:00:41 -0300

    iniciando componente com as mensagens

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ChannelData: React.FC = () => {
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+export default ChannelData;
