d42b19cc699f431cf501ee36270c48a65c1d3fc8
matheus souza <ruivorobado@gmail.com>
2021-10-09 09:12:43 -0300

    inciando mensagens

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { 
+  Container,
+  Messages,
+  InputWrapper,
+  Input,
+  InputIcon
+
+} from './styles';
 
 const ChannelData: React.FC = () => {
   return (
     <Container>
+      <Messages />
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversar em chat livre" />
+        <InputIcon />
+      </InputWrapper>
     </Container>
   );
 }
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,6 +8,7 @@ import ChannelInfo from '../ChannelInfo'
 import ChannelList from '../ChannelList';
 import UserInfo from '../UserInfo'
 import UserList from '../UserList';
+import ChannelData from '../ChannelData'
 
 const Layout: React.FC = () => {
   return (
@@ -17,8 +18,8 @@ const Layout: React.FC = () => {
       <ChannelInfo />
       <ChannelList />
       <UserInfo />
+      <ChannelData />
       <UserList />
-      {/* <div/> */}
     </Grid>
   );
 }
